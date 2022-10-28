3206a06b9c988f9f6fa5a1d43d151bdb66717e85
Casey Tucker <dctucker@hotmail.com>
2022-10-28 13:06:07 -0700

    resolve rust warning about height

--- src/main.rs
+++ src/main.rs
@@ -24,8 +24,8 @@ fn block_char(i:i8, inv:i8) -> String {
 }
 
 fn main() {
-	const height : i8 = 6;
-	let mut grid = vec![ vec![]; height as usize ];
+	const HEIGHT : i8 = 6;
+	let mut grid = vec![ vec![]; HEIGHT as usize ];
 
 	let args: Vec<String> = env::args().collect();
 	let path = &args[1];
@@ -36,23 +36,23 @@ fn main() {
 
 	println!("{}", path);
 	println!();
-	for n in 0..height/2 as i8 {
+	for n in 0..HEIGHT/2 as i8 {
 		grid[n as usize].push(String::from("\x1b[36m"));
-		grid[(n+(height/2)) as usize].push(String::from("\x1b[36m"));
+		grid[(n+(HEIGHT/2)) as usize].push(String::from("\x1b[36m"));
 	}
 	reader.samples::<i16>().for_each(|x| {
 		let sample = x.unwrap() as f32 / ( i16::MAX as f32);
 		if sample > max_samp { max_samp = sample; }
 		if sample < min_samp { min_samp = sample; }
 		if i % 5 == 4 {
-			let min_i = -(min_samp * 4.0 * (height as f32)) as i8;
-			let max_i =  (max_samp * 4.0 * (height as f32)) as i8;
+			let min_i = -(min_samp * 4.0 * (HEIGHT as f32)) as i8;
+			let max_i =  (max_samp * 4.0 * (HEIGHT as f32)) as i8;
 
-			for n in 0..height/2 as i8 {
-				let sub = ((height/2-1)-n) * 8 as i8;
+			for n in 0..HEIGHT/2 as i8 {
+				let sub = ((HEIGHT/2-1)-n) * 8 as i8;
 				grid[n as usize].push(block_char(min(8,max(0,min_i-sub)), 0));
 				let sub = n * 8 as i8;
-				grid[(n+(height/2)) as usize].push(block_char(8 - min(8,max(0,max_i-sub)), 1));
+				grid[(n+(HEIGHT/2)) as usize].push(block_char(8 - min(8,max(0,max_i-sub)), 1));
 			}
 			min_samp = 0.0;
 			max_samp = 0.0;
